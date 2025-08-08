85473053ab41c9fb61b8bacb85c2a2076919878e
Ray Ouyang <12910022+ohyung1287@users.noreply.github.com>
2025-08-08 12:33:14 -0600

    add more test for signature

--- test/Spacecoin.EthlessCancel.test.js
+++ test/Spacecoin.EthlessCancel.test.js
@@ -1,6 +1,5 @@
 require('dotenv');
 const { expect } = require('chai');
-const { ethers } = require('hardhat');
 const TestHelper = require('../shared/helper');
 const SignHelper = require('../shared/signature');
 let owner;
@@ -10,9 +9,7 @@ let user3;
 let spaceCoin;
 let provider;
 
-const feeToPay = 0;
-
-describe.only('SpaceCoin - Ethless Cancel Authorization functions', function () {
+describe('SpaceCoin - Ethless Cancel Authorization functions', function () {
     before(async () => {
         [provider, owner, user1, user2, user3] = await TestHelper.setupProviderAndWallet();
     });
@@ -76,63 +73,6 @@ describe.only('SpaceCoin - Ethless Cancel Authorization functions', function () 
                 )
             ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
         });
-
-        // it('Test Ethless cancel authorization when authorization is already used', async () => {
-        //     const nonce = TestHelper.generateNonce();
-            
-        //     // First, create and use an authorization
-        //     const [blockNumber] = await Promise.all([provider.getBlockNumber()]);
-        //     const block = await provider.getBlock(blockNumber);
-        //     const validAfter = block.timestamp;
-        //     const validBefore = block.timestamp + 20000;
-            
-        //     const transferSignature = await SignHelper.signTransfer(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         user2.address,
-        //         100,
-        //         validAfter,
-        //         validBefore,
-        //         nonce
-        //     );
-            
-        //     // Use the authorization
-        //     await spaceCoin.transferWithAuthorization(
-        //         owner.address,
-        //         user2.address,
-        //         100,
-        //         validAfter,
-        //         validBefore,
-        //         nonce,
-        //         transferSignature.v,
-        //         transferSignature.r,
-        //         transferSignature.s
-        //     );
-            
-        //     // Check that the authorization is now used
-        //     expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
-            
-        //     // Try to cancel an already used authorization
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce
-        //     );
-            
-        //     await expect(
-        //         spaceCoin.cancelAuthorization(
-        //             owner.address,
-        //             nonce,
-        //             cancelSignature.v,
-        //             cancelSignature.r,
-        //             cancelSignature.s
-        //         )
-        //     ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
-        // });
     });
 
     describe('SpaceCoin - Test expecting failure Ethless Cancel Authorization', async function () {
@@ -181,150 +121,126 @@ describe.only('SpaceCoin - Ethless Cancel Authorization functions', function () 
             ).to.be.revertedWithCustomError(spaceCoin, 'InvalidSignature');
         });
 
-        // it('Test Ethless cancel authorization with wrong nonce', async () => {
-        //     const nonce1 = TestHelper.generateNonce();
-        //     const nonce2 = TestHelper.generateNonce();
+        it('Test Ethless cancel authorization with wrong nonce', async () => {
+            const nonce1 = TestHelper.generateNonce();
+            const nonce2 = TestHelper.generateNonce();
             
-        //     // Create a cancel signature for nonce1
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce1
-        //     );
+            // Create a cancel signature for nonce1
+            const cancelSignature = await SignHelper.signCancel(
+                TestHelper.NAME,
+                TestHelper.VERSION_712,
+                await spaceCoin.getAddress(),
+                owner,
+                nonce1
+            );
             
-        //     // Try to cancel with wrong nonce
-        //     await expect(
-        //         spaceCoin.cancelAuthorization(
-        //             owner.address,
-        //             nonce2, // Wrong nonce
-        //             cancelSignature.v,
-        //             cancelSignature.r,
-        //             cancelSignature.s
-        //         )
-        //     ).to.be.revertedWithCustomError(spaceCoin, 'InvalidSignature');
-        // });
+            // Try to cancel with wrong nonce
+            await expect(
+                spaceCoin.cancelAuthorization(
+                    owner.address,
+                    nonce2, // Wrong nonce
+                    cancelSignature.v,
+                    cancelSignature.r,
+                    cancelSignature.s
+                )
+            ).to.be.revertedWithCustomError(spaceCoin, 'InvalidSignature');
+        });
 
-        // it('Test Ethless cancel authorization with tampered signature', async () => {
-        //     const nonce = TestHelper.generateNonce();
+        it('Test Ethless cancel authorization twice', async () => {
+            const nonce = TestHelper.generateNonce();
             
-        //     // Create a cancel signature
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce
-        //     );
+            // Create a cancel signature
+            const cancelSignature = await SignHelper.signCancel(
+                TestHelper.NAME,
+                TestHelper.VERSION_712,
+                await spaceCoin.getAddress(),
+                owner,
+                nonce
+            );
             
-        //     // Try to cancel with tampered signature (wrong v value)
-        //     await expect(
-        //         spaceCoin.cancelAuthorization(
-        //             owner.address,
-        //             nonce,
-        //             cancelSignature.v + 1, // Tampered v
-        //             cancelSignature.r,
-        //             cancelSignature.s
-        //         )
-        //     ).to.be.revertedWithCustomError(spaceCoin, 'InvalidSignature');
-        // });
+            // Cancel the authorization
+            await spaceCoin.cancelAuthorization(
+                owner.address,
+                nonce,
+                cancelSignature.v,
+                cancelSignature.r,
+                cancelSignature.s
+            );
+            
+            // Check that the authorization is now used
+            expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
+            
+            // Try to cancel the same authorization again
+            await expect(
+                spaceCoin.cancelAuthorization(
+                    owner.address,
+                    nonce,
+                    cancelSignature.v,
+                    cancelSignature.r,
+                    cancelSignature.s
+                )
+            ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
+        });
 
-        // it('Test Ethless cancel authorization twice', async () => {
-        //     const nonce = TestHelper.generateNonce();
+        it('Test Ethless cancel authorization for non-existent authorization', async () => {
+            const nonce = TestHelper.generateNonce();
             
-        //     // Create a cancel signature
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce
-        //     );
+            // Create a cancel signature
+            const cancelSignature = await SignHelper.signCancel(
+                TestHelper.NAME,
+                TestHelper.VERSION_712,
+                await spaceCoin.getAddress(),
+                owner,
+                nonce
+            );
             
-        //     // Cancel the authorization
-        //     await spaceCoin.cancelAuthorization(
-        //         owner.address,
-        //         nonce,
-        //         cancelSignature.v,
-        //         cancelSignature.r,
-        //         cancelSignature.s
-        //     );
+            // Cancel the authorization (this should work even for non-existent authorizations)
+            await spaceCoin.cancelAuthorization(
+                owner.address,
+                nonce,
+                cancelSignature.v,
+                cancelSignature.r,
+                cancelSignature.s
+            );
             
-        //     // Check that the authorization is now used
-        //     expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
-            
-        //     // Try to cancel the same authorization again
-        //     await expect(
-        //         spaceCoin.cancelAuthorization(
-        //             owner.address,
-        //             nonce,
-        //             cancelSignature.v,
-        //             cancelSignature.r,
-        //             cancelSignature.s
-        //         )
-        //     ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
-        // });
+            // Check that the authorization is now marked as used
+            expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
+        });
 
-        // it('Test Ethless cancel authorization for non-existent authorization', async () => {
-        //     const nonce = TestHelper.generateNonce();
+        it('Test Ethless cancel authorization with expired signature', async () => {
+            const nonce = TestHelper.generateNonce();
             
-        //     // Create a cancel signature
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce
-        //     );
+            // Create a cancel signature
+            const cancelSignature = await SignHelper.signCancel(
+                TestHelper.NAME,
+                TestHelper.VERSION_712,
+                await spaceCoin.getAddress(),
+                owner,
+                nonce
+            );
             
-        //     // Cancel the authorization (this should work even for non-existent authorizations)
-        //     await spaceCoin.cancelAuthorization(
-        //         owner.address,
-        //         nonce,
-        //         cancelSignature.v,
-        //         cancelSignature.r,
-        //         cancelSignature.s
-        //     );
+            // Cancel the authorization
+            await spaceCoin.cancelAuthorization(
+                owner.address,
+                nonce,
+                cancelSignature.v,
+                cancelSignature.r,
+                cancelSignature.s
+            );
             
-        //     // Check that the authorization is now marked as used
-        //     expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
-        // });
-
-        // it('Test Ethless cancel authorization with expired signature', async () => {
-        //     const nonce = TestHelper.generateNonce();
+            // Check that the authorization is now used
+            expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
             
-        //     // Create a cancel signature
-        //     const cancelSignature = await SignHelper.signCancel(
-        //         TestHelper.NAME,
-        //         TestHelper.VERSION_712,
-        //         await spaceCoin.getAddress(),
-        //         owner,
-        //         nonce
-        //     );
-            
-        //     // Cancel the authorization
-        //     await spaceCoin.cancelAuthorization(
-        //         owner.address,
-        //         nonce,
-        //         cancelSignature.v,
-        //         cancelSignature.r,
-        //         cancelSignature.s
-        //     );
-            
-        //     // Check that the authorization is now used
-        //     expect(await spaceCoin.authorizationState(owner.address, nonce)).to.be.true;
-            
-        //     // Try to cancel again with the same signature (should fail)
-        //     await expect(
-        //         spaceCoin.cancelAuthorization(
-        //             owner.address,
-        //             nonce,
-        //             cancelSignature.v,
-        //             cancelSignature.r,
-        //             cancelSignature.s
-        //         )
-        //     ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
-        // });
+            // Try to cancel again with the same signature (should fail)
+            await expect(
+                spaceCoin.cancelAuthorization(
+                    owner.address,
+                    nonce,
+                    cancelSignature.v,
+                    cancelSignature.r,
+                    cancelSignature.s
+                )
+            ).to.be.revertedWithCustomError(spaceCoin, 'AuthorizationUsedOrCanceled');
+        });
     });
 });
